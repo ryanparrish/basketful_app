89fd91d9a3084d10485d2d560eb72e4063e2a2e3
ryanparrish <parrish.ryan@gmail.com>
2025-12-01 07:29:55 -0500

    Update 0003_product_category_product_subcategory.py

--- apps/pantry/migrations/0003_product_category_product_subcategory.py
+++ apps/pantry/migrations/0003_product_category_product_subcategory.py
@@ -11,7 +11,7 @@ class Migration(migrations.Migration):
     ]
 
     operations = [
-        migrations.AlterField(
+        migrations.AddField(
             model_name='product',
             name='category',
             field=models.ForeignKey(blank=True, null=True, on_delete=django.db.models.deletion.SET_NULL, related_name='products', to='pantry.category'),
