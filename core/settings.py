32c210fabdeff1a914afd9ad75176b10f427a81b
ryanparrish <parrish.ryan@gmail.com>
2026-02-12 18:46:07 -0500

    Serve Bootstrap locally to fix CSP violations
    - Downloaded Bootstrap 5.3.3 CSS and JS to static/bootstrap/
    - Configured BOOTSTRAP5 settings to use local files instead of CDN
    - Added STATICFILES_DIRS to include static/ directory
    - Ran collectstatic to copy Bootstrap files to staticfiles/
    
    This resolves Content-Security-Policy errors that were blocking
    cdn.jsdelivr.net from loading Bootstrap. All assets now served
    from 'self' domain, passing CSP checks.

--- core/settings.py
+++ core/settings.py
@@ -156,6 +156,9 @@ if USE_S3:
 else:
     STATIC_URL = "/static/"
     STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+    STATICFILES_DIRS = [
+        os.path.join(BASE_DIR, "static"),
+    ]
 
     MEDIA_URL = "/media/"
     MEDIA_ROOT = os.path.join(BASE_DIR, "media")
@@ -391,6 +394,16 @@ CORS_ALLOWED_ORIGINS = env.list('CORS_ALLOWED_ORIGINS', default=[
     'http://127.0.0.1:5175',
 ])
 CORS_ALLOW_CREDENTIALS = True
+
+# Django Bootstrap 5 Configuration - Serve locally to avoid CSP violations
+BOOTSTRAP5 = {
+    'css_url': {
+        'url': '/static/bootstrap/bootstrap.min.css',
+    },
+    'javascript_url': {
+        'url': '/static/bootstrap/bootstrap.bundle.min.js',
+    },
+}
 
 # API Documentation (drf-spectacular)
 SPECTACULAR_SETTINGS = {
